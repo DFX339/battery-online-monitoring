f4630c63781b4de21b82236375bb5b2f4a66fd90
duanfeixia <chinalife@chinalife.com.hk>
2019-06-13 14:15:24 +0800

    这是mondrian编译时用到的jar包，将此目录替换 C:\Users\xxx 目录下的 .ivy2 目录

---
+++ src/main/java/com/gaunyi/batteryonline/common/BaseResult.java
@@ -0,0 +1,10 @@
+package com.gaunyi.batteryonline.common;
+
+/**
+ * Created by S0111 on 2019/6/13.
+ * 返回的结果集
+ */
+public class ResultSet {
+
+    private Map<String,>
+}
---
+++ src/main/java/com/gaunyi/batteryonline/common/DataResult.java
@@ -0,0 +1,7 @@
+package com.gaunyi.batteryonline.common;
+
+/**
+ * Created by S0111 on 2019/6/13.
+ */
+public class DataResult {
+}
